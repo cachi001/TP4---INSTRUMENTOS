cd4bdf188e3de264e516de2ee13ad0d80398544e
cachi <emilianito528@gmail.com>
2025-05-06 21:58:10 -0300

    TP5 ACTUALIZADO 2

--- frontend/instrumentos-front/src/components/Productos.tsx
+++ frontend/instrumentos-front/src/components/Productos.tsx
@@ -11,14 +11,14 @@ export const Productos = () => {
             <Header></Header>
 
             <section 
-            className='lg:py-10 lg:px-10 md:px-4 py-10 px-10 gap-4 grid grid-cols-[repeat(auto-fit,_minmax(400px,_1fr))] place-items-center'
+            className='lg:py-10 lg:px-10 md:px-4 py-10 px-10 gap-4 grid grid-cols-[repeat(auto-fit,_minmax(400px,1fr))] place-items-center'
             >
 
                 {productos!.map((producto, index) =>{
 
                     return(
                     <Link key={index} to={`/detalle/${producto.id}`}>
-                        <div className='lg:w-120 md:w-100 min-w-100 h-60 flex justify-center items-center border-1 lg:p-4 p-4 rounded-2xl border-gray-400 shadow-md hover:shadow-xl transition-shadow duration-300 lg:gap-10 md:gap-6 gap-10'>
+                        <div className='lg:w-110 md:w-100 min-w-100 h-60 flex justify-center items-center border-1 lg:p-4 p-4 rounded-2xl border-gray-400 shadow-md hover:shadow-xl transition-shadow duration-300 lg:gap-10 md:gap-6 gap-10'>
                             <div className='lg:w-40 w-30 rounded-2xl flex justify-center'>
                                 {producto.imagen.length > 30 ? (
                                     <img src={`${producto.imagen}`} alt="producto-imagen" className='w-fit lg:h-40 md:h-24 h-26' />
